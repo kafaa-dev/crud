d2c90ccd1dc1207e074d9b8c3bc3c8287e60f5a6
syahid246 <73120669+syahid246@users.noreply.github.com>
2020-10-20 02:03:41 +0700

    tampil.php

--- style/tampil.php
+++ style/tampil.php
@@ -1,1 +1,18 @@
-
+b {
+    font-family: arial;
+}
+.button {
+    outline: none;
+    display: inline-block;
+    font-family: arial;
+    background-color: #F3F4F5;
+    border: none;
+    color: black;
+    padding: 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    cursor: pointer;
+    border-radius: 12px;
+}
