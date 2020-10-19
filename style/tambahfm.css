38dd1ac1b2c3263fe97f844912459d2d94956199
syahid246 <73120669+syahid246@users.noreply.github.com>
2020-10-20 02:04:16 +0700

    tambahfm.php

---
+++ style/tambahfm.css
@@ -0,0 +1,51 @@
+input {
+    outline: none;
+}
+.bg {
+        border: none;
+        outline: none;
+        width: 380px;
+        height: 60px;
+        margin-top: -38px;
+        margin-left: 40px;
+        border-radius: 8px;
+        display: inline-block;
+}
+.as {
+        border: 1px solid #f3f4f5;
+        width: 400px;
+        height: 20px;
+        background-color: #f3f4f5;
+        padding: 20px;
+        text-align: left;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        border-radius: 8px;
+}
+
+.button {
+        outline: none;
+        display: inline-block;
+        font-family: arial;
+        background-color: #F3F4F5;
+        border: none;
+        color: black;
+        padding: 10px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 14px;
+        cursor: pointer;
+        border-radius: 12px;        
+    
+}
+
+div b {
+    color: rgba(0,0,0,.54);
+}
+
+td {
+        font-family: arial;
+}
