e6669a6e2bb1fce307bd4c19191be1698e8203d7
syahid246 <73120669+syahid246@users.noreply.github.com>
2020-10-21 23:29:29 +0700

    Simpan ubah pesanan

--- pesananubahsim.php
+++ pesananubahsim.php
@@ -7,12 +7,13 @@ $id_pesanan = $_GET['id_pesanan'];
 $nm_pengirim = $_GET['nm_pengirim'];
 $nm_penerima = $_GET['nm_penerima'];
 $nm_barang = $_GET['nm_barang'];
+$marketplace = $_GET['marketplace'];
 $jasa_kirim = $_GET['jasa_kirim'];
 $harga = $_GET['harga'];
 $jumlah_beli = $_GET['jumlah_beli'];
 $alamat  = $_GET['alamat'];
 
-$update = "UPDATE pesanan SET nm_pengirim='$nm_pengirim', nm_penerima='$nm_penerima', nm_barang='$nm_barang', jasa_kirim='$jasa_kirim', harga='$harga', jumlah_beli='$jumlah_beli', alamat='$alamat' WHERE id_pesanan='$id'";
+$update = "UPDATE pesanan SET nm_pengirim='$nm_pengirim', nm_penerima='$nm_penerima', nm_barang='$nm_barang', marketplace='$marketplace', jasa_kirim='$jasa_kirim', harga='$harga', jumlah_beli='$jumlah_beli', alamat='$alamat' WHERE id_pesanan='$id'";
 $hasil  = mysqli_query($konek, $update);
 
 // apabila query untuk mengupdate data benar
